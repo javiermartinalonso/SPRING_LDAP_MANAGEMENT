0dc74b8f465f3d8886b4c5fd42fcb5366f72a4b1
Javier Martín Alonso <xavimartin@hotmail.com>
2016-10-13 23:27:40 +0200

    actualizado para test

--- ldap-management-web/src/main/java/es/ourorganization/ldap/web/management/view/controller/RestController.java
+++ ldap-management-web/src/main/java/es/ourorganization/ldap/web/management/view/controller/RestController.java
@@ -2,8 +2,6 @@ package es.ourorganization.ldap.web.management.view.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.naming.Name;
 
@@ -15,18 +13,14 @@ import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.ldap.samples.utils.HtmlRowLdapTreeVisitor;
 import org.springframework.ldap.samples.utils.LdapTree;
 import org.springframework.ldap.samples.utils.LdapTreeBuilder;
+import org.springframework.ldap.samples.utils.NodoLDAPTreeVisitor;
 import org.springframework.ldap.support.LdapUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import es.ourorganization.ldap.web.management.modelo.bean.NodeChildren;
 import es.ourorganization.ldap.web.management.modelo.bean.NodeRoot;
-import es.ourorganization.ldap.web.management.modelo.bean.NodoLDAP;
-import es.ourorganization.ldap.web.management.modelo.bean.SubTypeNode;
-import es.ourorganization.ldap.web.management.modelo.bean.TypeNode;
 import es.ourorganization.ldap.web.management.util.InternationalizationUtil;
 
 /**
@@ -77,7 +71,7 @@ public class RestController
 	 * @throws CoreException
 	 */
 	@RequestMapping(value = "/organizacion", method = RequestMethod.GET)
-	public @ResponseBody NodeRoot getExpedientesByAplicacion()
+	public @ResponseBody String getExpedientesByAplicacion()
 	{
 		LOGGER.info("Muestro la organizacion");
 
@@ -85,48 +79,52 @@ public class RestController
 		LdapTree ldapTree = ldapTreeBuilder.getLdapTree(LdapUtils.emptyLdapName());
 
 		//recorremos el arbol buscando los nodos de personas y monto una lista con los enlaces rest/htmls a la informacion de cada nodo
-		HtmlRowLdapTreeVisitor visitor = new PersonLinkHtmlRowLdapTreeVisitor();
+//		HtmlRowLdapTreeVisitor visitor = new PersonLinkHtmlRowLdapTreeVisitor();
+		NodoLDAPTreeVisitor visitor = new NodoLDAPTreeVisitor(new Long(2));
+		
 		//recorre el arbol LDAP y genera una lista en el objeto de introspeccion del arbol con los nodos en formato de links de html
 		ldapTree.traverse(visitor);
 		//lista donde cada elemento representa un link a la informacion de una persona definida en el LDAP
-		List<String> arbolLDAP = visitor.getRows();
-		//		return new ModelAndView("showTree", "rows", visitor.getRows());
+		NodeRoot nodeRoot = visitor.getNodeRoot();
 
-		NodoLDAP nodoLDAP1 = new NodoLDAP("2435", "collateral", TypeNode.collateral, "collateral", null, null, null, null);
-
-		NodoLDAP nodoLDAP2 = new NodoLDAP("2436", "staff", TypeNode.staff, "staff", null, null, null, null);
-		NodoLDAP nodoLDAP3 = new NodoLDAP("2437", "staffleft", TypeNode.staffleft, "staffleft", "leon.jpg", "below", "#FFFFFF", SubTypeNode.dashed);
-		NodoLDAP nodoLDAP4 = new NodoLDAP("2438", "stafftop", TypeNode.stafftop, "stafftop", null, null, null, null);
-		NodoLDAP nodoLDAP5 = new NodoLDAP("2439", "subordinate", TypeNode.subordinate, "subordinate", null, null, null, null);
-		NodoLDAP nodoLDAP6 = new NodoLDAP("2439", "undefined", TypeNode.undefined, "undefined", null, null, null, null);
-
-		List<NodoLDAP> listNodoLDAP = new LinkedList<NodoLDAP>();
-		listNodoLDAP.add(nodoLDAP1);
-		listNodoLDAP.add(nodoLDAP2);
-		listNodoLDAP.add(nodoLDAP3);
-		listNodoLDAP.add(nodoLDAP4);
-		listNodoLDAP.add(nodoLDAP5);
-		listNodoLDAP.add(nodoLDAP6);
-
-		NodeChildren nodeChildren = new NodeChildren("41", "MINISTERIO", TypeNode.staff, "ministerio", null, null, "#FFFFFF", SubTypeNode.dashed, listNodoLDAP);
-
-		NodeRoot nodeRoot = new NodeRoot(new Long(2), "Nosotros", nodeChildren);
-
-		//    	NodoLDAP nodoLDAP1 = new NodoLDAP("140", "Abuela", TypeNode.staff, "subtitle", "http://static.batanga.com/sites/default/files/styles/full/public/universo-observable-en-una-imagen-3.png", "", "", null);
-		//    	NodoLDAP nodoLDAP2 = new NodoLDAP("139", "Abuelo", TypeNode.staff, "subtitle", "./img/leon.jpg", "below", "", null);
-		//    	NodoLDAP nodoLDAP3 = new NodoLDAP("108", "Padre", TypeNode.subordinate, "subtitle", "./img/burro.jpg", "above", "", null);
-		//    	NodoLDAP nodoLDAP4 = new NodoLDAP("109", "Madre", TypeNode.subordinate, "", "./img/raton.jpg", "above", "", null);
-		//    	
-		//    	List <NodoLDAP> listNodoLDAP = new LinkedList<NodoLDAP>();
-		//    	listNodoLDAP.add(nodoLDAP1);
-		//    	listNodoLDAP.add(nodoLDAP2);
-		//    	listNodoLDAP.add(nodoLDAP3);
-		//    	listNodoLDAP.add(nodoLDAP4);
-		//
-		//    
-		//    	NodeChildren nodeChildren = new NodeChildren("107", "Familia", TypeNode.staff, "Familia", "https://image.freepik.com/foto-gratis/hrc-tigre-siberiano-2-jpg_21253111.jpg", "below", "#FFFFFF", SubTypeNode.dashed, listNodoLDAP);
-		//    	
-		//    	NodeRoot nodeRoot = new NodeRoot(new Long(2), "Nosotros", nodeChildren);
+//		NodoLDAP nodoLDAP1 = new NodoLDAP("2435", "collateral", TypeNode.collateral, "collateral", null, null, null, null);
+//
+//		NodoLDAP nodoLDAP2 = new NodoLDAP("2436", "staff", TypeNode.staff, "staff", null, null, null, null);
+//		NodoLDAP nodoLDAP3 = new NodoLDAP("2437", "staffleft", TypeNode.staffleft, "staffleft", "leon.jpg", "below", "#FFFFFF", SubTypeNode.dashed);
+//		NodoLDAP nodoLDAP4 = new NodoLDAP("2438", "stafftop", TypeNode.stafftop, "stafftop", null, null, null, null);
+//		NodoLDAP nodoLDAP5 = new NodoLDAP("2439", "subordinate", TypeNode.subordinate, "subordinate", null, null, null, null);
+//		NodoLDAP nodoLDAP6 = new NodoLDAP("2439", "undefined", TypeNode.undefined, "undefined", null, null, null, null);
+//
+//		List<NodoLDAP> listNodoLDAP = new LinkedList<NodoLDAP>();
+//		listNodoLDAP.add(nodoLDAP1);
+//		listNodoLDAP.add(nodoLDAP2);
+//		listNodoLDAP.add(nodoLDAP3);
+//		listNodoLDAP.add(nodoLDAP4);
+//		listNodoLDAP.add(nodoLDAP5);
+//		listNodoLDAP.add(nodoLDAP6);
+//
+//		NodeChildren nodeChildren = new NodeChildren("41", "MINISTERIO", TypeNode.staff, "ministerio", null, null, "#FFFFFF", SubTypeNode.dashed, listNodoLDAP);
+//
+//		NodeRoot nodeRoot = new NodeRoot(new Long(2), "Nosotros", nodeChildren);
+		
+		
+		
+//
+//		    	NodoLDAP nodoLDAP1 = new NodoLDAP("140", "Abuela", TypeNode.staff, "subtitle", "http://static.batanga.com/sites/default/files/styles/full/public/universo-observable-en-una-imagen-3.png", "", "", null);
+//		    	NodoLDAP nodoLDAP2 = new NodoLDAP("139", "Abuelo", TypeNode.staff, "subtitle", "./img/leon.jpg", "below", "", null);
+//		    	NodoLDAP nodoLDAP3 = new NodoLDAP("108", "Padre", TypeNode.subordinate, "subtitle", "./img/burro.jpg", "above", "", null);
+//		    	NodoLDAP nodoLDAP4 = new NodoLDAP("109", "Madre", TypeNode.subordinate, "", "./img/raton.jpg", "above", "", null);
+//		    	
+//		    	List <NodoLDAP> listNodoLDAP = new LinkedList<NodoLDAP>();
+//		    	listNodoLDAP.add(nodoLDAP1);
+//		    	listNodoLDAP.add(nodoLDAP2);
+//		    	listNodoLDAP.add(nodoLDAP3);
+//		    	listNodoLDAP.add(nodoLDAP4);
+//		
+//		    
+//		    	NodeChildren nodeChildren = new NodeChildren("107", "Familia", TypeNode.staff, "Familia", "https://image.freepik.com/foto-gratis/hrc-tigre-siberiano-2-jpg_21253111.jpg", "below", "#FFFFFF", SubTypeNode.dashed, listNodoLDAP);
+//		    	
+//		    	nodeRoot = new NodeRoot(new Long(2), "Nosotros", nodeChildren);
 		//    	
 		//    	"id" : 1,
 		//    	"title" : "Nosotros",
@@ -141,7 +139,7 @@ public class RestController
 		//    		]
 		//    	}
 
-		return nodeRoot;
+		return nodeRoot.toString();
 
 	}
 
@@ -162,7 +160,7 @@ public class RestController
 	
 	/**
 	 * Clase que extiende de HtmlRowLdapTreeVisitor, 
-	 * sobreescribiendo la forma en la que generamos los urls de acceso a la información de un nodo.
+	 * sobreescribiendo la forma en la que generamos los urls de acceso a la informacion de un nodo.
 	 * Generates appropriate links for person leaves in the tree. Recorre el
 	 * arbol buscando los nodos de personas y monto una lista con los enlaces
 	 * rest/htmls a la informacion de cada nodo
